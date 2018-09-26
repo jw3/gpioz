d5b36154e2da267cf8bb2d7480a6204726a7885d
John Wass <jwass3@gmail.com>
2018-09-25 22:33:28 -0400

    move example to spec

--- src/main/scala/gpioz/Gpioz.scala
+++ src/main/scala/gpioz/Gpioz.scala
@@ -15,37 +15,3 @@ object Gpioz {
   type ConfigGet[O] = PinReader[O]
   type ConfigSet[I] = PinWriter[I]
 }
-
-object Fakez {
-  implicit def fakeReader(p: UserGpio): GpIO[Level] = {
-    for {
-      _ ← IO.sync(println("fake reading"))
-      res ← IO.now(High)
-    } yield res
-  }
-
-  implicit def fakeWriter(p: UserGpio, l: Level): GpIO[GpioResult] = {
-    for {
-      _ ← IO.sync(println("fake writing"))
-      res ← IO.now(GpioOk)
-    } yield res
-  }
-}
-
-object Test extends zio.App {
-  ExitStatus.ExitNow(1)
-
-  def run(args: List[String]): IO[Nothing, Test.ExitStatus] =
-    fn.attempt.map(_.fold(_ => 1, _ => 0)).map(ExitStatus.ExitNow(_))
-
-  import Fakez._
-
-  def fn: IO[GpioFailure, Unit] = {
-    val level = High
-
-    for {
-      _ ← DefaultDigitalIO.gpioWrite(UserGpio(4), level)
-      r2 ← DefaultDigitalIO.gpioRead(UserGpio(5))
-    } yield r2 == level
-  }
-}
--- src/test/scala/gpioz/GpiozSpec.scala
+++ src/test/scala/gpioz/GpiozSpec.scala
@@ -1,3 +1,59 @@
 package gpioz
 
-object GpiozSpec {}
+import gpioz.Gpioz.{GpIO, GpInitRes}
+import gpioz.api._
+import org.scalatest.{Matchers, WordSpecLike}
+import scalaz.zio.{IO, RTS}
+
+object Fakez {
+  implicit def fakeInitz(): GpInitRes = {
+    for {
+      _ ← IO.sync(println("fake init"))
+      res ← InitResult(100)
+    } yield res
+  }
+
+  implicit def fakeReader(p: UserGpio): GpIO[Level] = {
+    for {
+      _ ← IO.sync(println("fake reading"))
+      res ← IO.now(High)
+    } yield res
+  }
+
+  implicit def fakeWriter(p: UserGpio, l: Level): GpIO[GpioResult] = {
+    for {
+      _ ← IO.sync(println("fake writing"))
+      res ← IO.now(GpioOk)
+    } yield res
+  }
+}
+
+class GpiozSpec extends RTS with WordSpecLike with Matchers with Initializer with DigitalIO {
+
+  import Fakez._
+
+  "fake gpioz" should {
+    "init" in {
+      unsafeRun(
+        for {
+          res ← gpioInitialise()(Fakez.fakeInitz)
+        } yield res shouldBe Init(100)
+      )
+    }
+    "read" in {
+      val expected = High
+      unsafeRun(
+        for {
+          level ← gpioRead(UserGpio(4))
+        } yield level shouldBe expected
+      )
+    }
+    "write" in {
+      unsafeRun(
+        for {
+          res ← gpioWrite(UserGpio(4), High)
+        } yield res shouldBe GpioOk
+      )
+    }
+  }
+}
